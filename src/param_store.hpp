345fcb90ed279788e3c14b1976acd04e91cd498f
Wojciech Migda <wojtek.golf@interia.pl>
2016-03-03 22:50:02 +0100

    more history data

--- src/param_store.hpp
+++ src/param_store.hpp
@@ -30,10 +30,10 @@ namespace params
 
 const std::map<const std::string, const std::string> CURRENT
 {
-//        {"booster", "gblinear"},
-    {"booster", "gbtree"}, // default
+//    {"booster", "gblinear"},
+    {"booster", "gbtree"},
     {"reg_alpha", "0"},
-    {"colsample_bytree", "0.7709"},
+    {"colsample_bytree", "0.65"},
     {"silent", "1"},
     {"colsample_bylevel", "1"},
     {"scale_pos_weight", "1"},
@@ -42,18 +42,177 @@ const std::map<const std::string, const std::string> CURRENT
     {"max_delta_step", "0"},
     {"base_score", "0.5"},
     {"n_estimators", "600"},
-    {"subsample", "0.6549"},
+    {"subsample", "0.85"},
     {"reg_lambda", "1"},
     {"seed", "0"},
-    {"min_child_weight", "85"},
+    {"min_child_weight", "65"},
 
     {"objective", "rank:pairwise"},
-//        {"objective", "binary:logitraw"},
-//        {"objective", "binary:logistic"},
-    {"max_depth", "5"},
-    {"gamma", "0.7745"}
+    {"num_pairsample", "2"},
+//    {"objective", "binary:logitraw"},
+//    {"objective", "binary:logistic"},
+    {"max_depth", "7"},
+    {"gamma", "0"}
 };
 
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+const std::map<const std::string, const std::string> sub8
+{
+    // LB: 811038.67
+    // CV: 810404
+    // no FE
+
+    {"booster", "gbtree"},
+    {"reg_alpha", "0"},
+    {"colsample_bytree", "0.65"},
+    {"silent", "1"},
+    {"colsample_bylevel", "1"},
+    {"scale_pos_weight", "1"},
+    {"learning_rate", "0.045"},
+    {"missing", "nan"},
+    {"max_delta_step", "0"},
+    {"base_score", "0.5"},
+    {"n_estimators", "600"},
+    {"subsample", "0.85"},
+    {"reg_lambda", "1"},
+    {"seed", "0"},
+    {"min_child_weight", "65"},
+
+    {"objective", "rank:pairwise"},
+    {"max_depth", "7"},
+    {"gamma", "0"}
+};
+
+
+const std::map<const std::string, const std::string> sub33
+{
+    // sub8, n_estimators=590
+
+    // LB: 810787.55
+    // CV: 811512
+    // no FE
+
+    {"booster", "gbtree"},
+    {"reg_alpha", "0"},
+    {"colsample_bytree", "0.65"},
+    {"silent", "1"},
+    {"colsample_bylevel", "1"},
+    {"scale_pos_weight", "1"},
+    {"learning_rate", "0.045"},
+    {"missing", "nan"},
+    {"max_delta_step", "0"},
+    {"base_score", "0.5"},
+    {"n_estimators", "590"},
+    {"subsample", "0.85"},
+    {"reg_lambda", "1"},
+    {"seed", "0"},
+    {"min_child_weight", "65"},
+
+    {"objective", "rank:pairwise"},
+    {"max_depth", "7"},
+    {"gamma", "0"}
+};
+
+
+const std::map<const std::string, const std::string> sub26
+{
+    // sub8, n_estimators=610
+
+    // LB: 810682.24
+    // CV: 810784
+    // no FE
+
+    {"booster", "gbtree"},
+    {"reg_alpha", "0"},
+    {"colsample_bytree", "0.65"},
+    {"silent", "1"},
+    {"colsample_bylevel", "1"},
+    {"scale_pos_weight", "1"},
+    {"learning_rate", "0.045"},
+    {"missing", "nan"},
+    {"max_delta_step", "0"},
+    {"base_score", "0.5"},
+    {"n_estimators", "610"},
+    {"subsample", "0.85"},
+    {"reg_lambda", "1"},
+    {"seed", "0"},
+    {"min_child_weight", "65"},
+
+    {"objective", "rank:pairwise"},
+    {"max_depth", "7"},
+    {"gamma", "0"}
+};
+
+
+const std::map<const std::string, const std::string> sub24
+{
+    // from hyperopt log 4 [10-fold] (rank 4th) where it scored 813247.42337
+
+    // LB: 810479.86
+    // CV: 809573
+    // no FE
+
+    {"booster", "gbtree"},
+    {"reg_alpha", "0"},
+    {"colsample_bytree", "0.4590"},
+    {"silent", "1"},
+    {"colsample_bylevel", "1"},
+    {"scale_pos_weight", "1"},
+    {"learning_rate", "0.045"},
+    {"missing", "nan"},
+    {"max_delta_step", "0"},
+    {"base_score", "0.5"},
+    {"n_estimators", "720"},
+    {"subsample", "0.6005"},
+    {"reg_lambda", "1"},
+    {"seed", "0"},
+    {"min_child_weight", "130"},
+
+        {"num_pairsample", "2"},
+
+    {"objective", "rank:pairwise"},
+    {"max_depth", "6"},
+    {"gamma", "0"}
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+
+const std::map<const std::string, const std::string> sub18
+{
+    // sub8, two FE: PROP_PAGE_IMPRESSIONS_DWELL, PROP_VOD_VIEWS_DWELL
+
+    // LB: 810488.63
+    // CV: 810224
+    // FE (only top-2)
+
+    {"booster", "gbtree"},
+    {"reg_alpha", "0"},
+    {"colsample_bytree", "0.65"},
+    {"silent", "1"},
+    {"colsample_bylevel", "1"},
+    {"scale_pos_weight", "1"},
+    {"learning_rate", "0.045"},
+    {"missing", "nan"},
+    {"max_delta_step", "0"},
+    {"base_score", "0.5"},
+    {"n_estimators", "600"},
+    {"subsample", "0.85"},
+    {"reg_lambda", "1"},
+    {"seed", "0"},
+    {"min_child_weight", "65"},
+
+    {"objective", "rank:pairwise"},
+    {"max_depth", "7"},
+    {"gamma", "0"}
+};
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 
 }  // namespace params
 
